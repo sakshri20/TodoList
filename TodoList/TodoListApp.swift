413d94f5f349944edfc339877efee3ed8ae16832
Sakshi Shrivastava <sak.20.shrivastava@gmail.com>
2026-03-05 15:20:31 -0800

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Sakshi Shrivastava on 3/5/26.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
